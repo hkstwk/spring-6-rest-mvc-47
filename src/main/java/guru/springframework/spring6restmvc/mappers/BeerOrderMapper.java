3a9539046b41aba3866ce01b6aead6f2c420a17d
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-09-29 10:31:49 +0200

    420 - Create Mappers

---
+++ src/main/java/guru/springframework/spring6restmvc/mappers/BeerOrderMapper.java
@@ -0,0 +1,36 @@
+package guru.springframework.spring6restmvc.mappers;
+
+import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.entities.BeerOrderLine;
+import guru.springframework.spring6restmvc.entities.BeerOrderShipment;
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderLineDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderShipmentDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper
+public interface BeerOrderMapper {
+
+    @Mapping(target = "beerOrderLines", source = "beerOrderLineDTOSet")
+    @Mapping(target = "beerOrderShipment", source = "beerOrderShipmentDTO")
+    @Mapping(target = "customer", source = "customerDTO")
+    BeerOrder beerOrderDtoToBeer(BeerOrderDTO beerOrderDTO);
+
+    @Mapping(target = "beerOrderLineDTOSet", source = "beerOrderLines")
+    @Mapping(target = "beerOrderShipmentDTO", source = "beerOrderShipment")
+    @Mapping(target = "customerDTO", source = "customer")
+    BeerOrderDTO beerOrderToBeerOrderDto(BeerOrder beerOrder);
+
+    @Mapping(target = "beerDTO", source = "beer")
+    BeerOrderLineDTO beerOrderLineToBeerOrderLineDto(BeerOrderLine beerOrderLine);
+
+    @Mapping(target = "beerOrder", ignore = true)
+    @Mapping(target = "beer", source = "beerDTO")
+    BeerOrderLine beerOrderLineDtoToBeerOrderLine(BeerOrderLineDTO beerOrderLineDTO);
+
+    BeerOrderShipmentDTO beerOrderShipmentToBeerOrderShipmentDto(BeerOrderShipment beerOrderShipment);
+
+    @Mapping(target = "beerOrder", ignore = true)
+    BeerOrderShipment beerOrderShipMentDtoToBeerOrderShipment(BeerOrderShipmentDTO beerOrderShipmentDTO);
+}
