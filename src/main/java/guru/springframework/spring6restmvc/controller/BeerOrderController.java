39c8b313a9f380c3f895ef0fd192e572b74ddfca
Harm Kolvoort <hkolvoort+github@gmail.com>
2024-09-29 22:37:25 +0200

    423 - Implement Get Operations (listBeerOrders)

--- src/main/java/guru/springframework/spring6restmvc/controller/BeerOrderController.java
+++ src/main/java/guru/springframework/spring6restmvc/controller/BeerOrderController.java
@@ -1,7 +1,14 @@
 package guru.springframework.spring6restmvc.controller;
 
+import guru.springframework.spring6restmvc.model.BeerDTO;
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import guru.springframework.spring6restmvc.model.BeerStyle;
+import guru.springframework.spring6restmvc.services.BeerOrderService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -10,4 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class BeerOrderController {
     public static final String BEER_ORDER_PATH = "/api/v1/beerorder";
     public static final String BEER_ORDER_PATH_ID = BEER_ORDER_PATH + "/{beerOrderId}";
+
+    private final BeerOrderService beerOrderService;
+
+    @GetMapping(value = BEER_ORDER_PATH)
+    public Page<BeerOrderDTO> listBeers(@RequestParam(required = false) Integer pageNumber,
+                                        @RequestParam(required = false) Integer pageSize){
+        return beerOrderService.listBeerOrders(pageNumber, pageSize);
+    }
 }
---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerOrderServiceJPA.java
@@ -0,0 +1,66 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.entities.Beer;
+import guru.springframework.spring6restmvc.entities.BeerOrder;
+import guru.springframework.spring6restmvc.mappers.BeerOrderMapper;
+import guru.springframework.spring6restmvc.model.BeerOrderDTO;
+import guru.springframework.spring6restmvc.repositories.BeerOrderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BeerOrderServiceJPA implements BeerOrderService {
+    private final BeerOrderRepository beerOrderRepository;
+    private final BeerOrderMapper beerOrderMapper;
+
+    private static final int DEFAULT_PAGE = 0;
+    private static final int DEFAULT_PAGE_SIZE = 25;
+
+    @Override
+    public Page<BeerOrderDTO> listBeerOrders(Integer pageNumber, Integer pageSize) {
+        PageRequest pageRequest = buildPageRequest(pageNumber, pageSize);
+
+        Page<BeerOrder> beerOrderPage =beerOrderRepository.findAll(pageRequest);
+
+        return beerOrderPage.map(beerOrderMapper::beerOrderToBeerOrderDto);
+    }
+
+    @Override
+    public Optional<BeerOrder> getBeerOrderById(UUID id) {
+        return Optional.empty();
+    }
+
+    public PageRequest buildPageRequest(Integer pageNumber, Integer pageSize) {
+        int queryPageNumber;
+        int queryPageSize;
+
+        if (pageNumber != null && pageNumber > 0) {
+            queryPageNumber = pageNumber - 1;
+        } else {
+            queryPageNumber = DEFAULT_PAGE;
+        }
+
+        if (pageSize == null) {
+            queryPageSize = DEFAULT_PAGE_SIZE;
+        } else {
+            if (pageSize > 1000) {
+                queryPageSize = 1000;
+            } else {
+                queryPageSize = pageSize;
+            }
+        }
+
+        Sort sort = Sort.by(Sort.Order.asc("createdDate"));
+
+        return PageRequest.of(queryPageNumber, queryPageSize, sort);
+    }
+}
